90c06f8f016c2774e95a24981139b720dbe55c60
RobertLemos14 <95971607+RobertLemos14@users.noreply.github.com>
2022-04-22 14:38:38 -0300

    Create exercicio4.py

---
+++ exercicio4.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome")
+dia = input("Digite o dia em que nasceu")
+mes = input("Digite o mês em que nasceu")
+ano = input("Digite o ano em que nasceu")
+
+texto = (f"{nome} nasceu no dia: {dia}/{mes}/{ano}")
+print(texto.capitalize())
